abc1172dc063035ced44582516ce878439faa240
NguyenMinhThien <nguyenminhthienph1449@gmail.com>
2025-04-12 08:55:52 +0700

    8:55 12/4/2025

---
+++ video9/video9.js
@@ -0,0 +1,17 @@
+console.log("Hello world !")
+
+let project= {
+    name: 'Nguyễn Minh Thiện',
+    age: 19
+}
+
+console.log(project)
+
+let array =[
+    project,
+    "Phan Bích hợp"
+]
+
+console.log(array)
+console.log(array[0].name)
+console.log(array[0].age)
